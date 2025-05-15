88857976f55ed50992b7acee0c3a731af82e4ad9
Junaid Khan <pathanjunaid7888@gmail.com>
2025-05-16 00:39:39 +0530

    CartItem.jsx
    new feature added

--- CartItem.jsx
+++ CartItem.jsx
@@ -1,5 +1,19 @@
 
+import { useState } from "react"
+
 function CartItem({item,deleteCartItem}) {
+const  [qty, setQty] = useState(1);
+ const inc = () => {
+    if (qty < 10) {
+      setQty(qty + 1);
+    }
+  };
+  const dec = () => {
+    if (qty > 1) {
+      setQty(qty - 1);
+    }
+  };
+   
   return (<>
     <div className='cartItem'>
       <h4><span className="imgThumbnail">
@@ -7,10 +21,12 @@ function CartItem({item,deleteCartItem}) {
        <sup style={{marginLeft:10,}}>{item.title}</sup>
        <button className="del-btn" onClick={deleteCartItem}>X</button>
        </h4>
-      <span className='Qty'>Qty:<button>-</button> 1 <button>+</button><span className="price">${item.price}</span></span>
+      <span className='Qty'>Qty:<button onClick={dec}>-</button> {qty} <button onClick={inc}>+</button>
+        <span className="price">${(item.price*qty).toFixed(2)}</span></span>
     </div>
     </>
   )
 }
 
 export default CartItem
+
